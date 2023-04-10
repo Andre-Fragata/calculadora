71692993547e72aef5f38218fca9d03da6016033
Fragante01 <andrefragata14@gmail.com>
2023-04-10 20:13:31 -0300

    Commit inicial, projeto calculadora

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import  './global.css'
+import Calculadora from './mainpage/Calculadora'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Calculadora />
+    </div>
+  )
+}
+
+export default App
